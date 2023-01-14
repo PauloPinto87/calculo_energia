a47f8a31083fdd5ba421c2041b0cd496819c68f7
Paulo Pinto <gomescasador@gmail.com>
2023-01-14 21:23:15 +0000

    JavaScript e responsividade finalizados. Projeto funcional e pronto.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -27,26 +27,36 @@ function consultar() {
         {empresa:'Endesa', kWh:0.1450, energContrat:0.2943, MIBEL: 0.02},
         {empresa:'GALP', kWh:0.1415, energContrat:0.1829, MIBEL: 0.04},
     ]
-    //Looping aplicando o nome, preço do kWh e da potência contratada.
-    for ({empresa, kWh, energContrat, MIBEL} of listaEmpresas) {
-        // Verifica o quanto passa de 100 kWh de consumo
-        if (consumo > 100) {
-            var consumo13 = 100
-            var consumo23 = Math.abs(consumo-100)
-        }
-        else {
-            var consumo13 = consumo
-        }
-        //Aplica os impostos expecíficos nas variáveis específicas
-        tudoIVA23 = (energContrat*30 + DGEG + impEspeEnerg + (kWh*consumo23) + (MIBEL*consumo))*1.23
-        tudoIVA13 = (consumo13 * kWh)*1.13
-        tudoIVA6 = (contrAudioVisu)*1.06
-        //Soma os valores sem e com impostos de todos os níveis!
-        total = tudoIVA23 + tudoIVA13 + tudoIVA6
-        //Escreve no DOM o resultado
-        document.querySelector('#idivresult').innerHTML += `${empresa} = ${total.toFixed(2)} €<br>`
+    //Teste caso o consumo zero ou negativo.
+    if (consumo < 0) {
+        document.querySelector('#idivresult').innerHTML = '[-ERRO-] - Valor de consumo informado não valido. Tente novamente!'
+        document.querySelector('#idivresult').style.color = 'red'
+    }
+    else {
+        //Looping aplicando o nome, preço do kWh e da potência contratada.
+        document.querySelector('#idivresult').style.color = 'black'
+        document.querySelector('#idivresult').innerHTML = `O consumo informado foi <strong>${consumo}kWh</strong>, segue abaixo as ampresas e os valores aproximados: <br> <br>`
+        for ({empresa, kWh, energContrat, MIBEL} of listaEmpresas) {
+            // Verifica o quanto passa de 100 kWh de consumo
+            if (consumo > 100) {
+                var consumo13 = 100
+                var consumo23 = Math.abs(consumo-100)
+            }
+            else {
+                var consumo13 = consumo
+                var consumo23 = ''
+            }
+            //Aplica os impostos expecíficos nas variáveis específicas
+            tudoIVA23 = (energContrat*30 + DGEG + impEspeEnerg + (kWh*consumo23) + (MIBEL*consumo))*1.23
+            tudoIVA13 = (consumo13 * kWh)*1.13
+            tudoIVA6 = (contrAudioVisu)*1.06
+            //Soma os valores sem e com impostos de todos os níveis!
+            total = tudoIVA23 + tudoIVA13 + tudoIVA6
+            //Escreve no DOM o resultado
+            document.querySelector('#idivresult').innerHTML += `${empresa} = ${total.toFixed(2)} €<br>`
 
     }
-    alert(listaEmpresas.has('Endesa'))   
+    }
+       
 
 }
