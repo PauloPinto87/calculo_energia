3535d1759a2248acdc25d0c3256529ba6fa285a1
Paulo Pinto <gomescasador@gmail.com>
2023-01-14 15:47:52 +0000

    Calculos validados (sem os loopings)

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,27 +1,48 @@
 function consultar() {
     var consumo = Number(document.querySelector('#iconsumo').value)
+
     var impEspeEnerg = consumo * 0.001
-    var IVA6 = 0.06
-    var IVA23 = 0.23
+    var energContrat = ''
     var kWh = ''
+    var empresa = ''
+    var MIBEL = ''
+    var totalImposto = ''
+    var tudoIVA23 = ''
+    var tudoIVA13 = ''
+    var tudoIVA6 = ''
+
     const contrAudioVisu = 2.85
     const DGEG = 0.07
 
-     //Luzboa
-    
-     var kWh = 0.2031
 
-    if (consumo < 3000) {
-        alert(`O IVA nessa conta de luz será ${ kWh * consumo * IVA23}`)
+    // Inicio
+    //Variaveis dessa empresa
+    empresa = 'EZU Energia'
+    energContrat = 0.1557
+    kWh = 0.1372
+    MIBEL = 0.04
+    //PODERIA COMEÇAR O LOOPING
+    // Verifica o quanto passa de 100 kWh de consumo
+    if (consumo > 100) {
+        var consumo13 = 100
+        var consumo23 = Math.abs(consumo-100)
     }
+    else {
+        var consumo13 = consumo
+    }
+    //Aplica os impostos expecíficos nas variáveis específicas
+    tudoIVA23 = (energContrat*30 + DGEG + impEspeEnerg + (kWh*consumo23) + (MIBEL*consumo))*1.23
+    tudoIVA13 = (consumo13 * kWh)*1.13
+    tudoIVA6 = (contrAudioVisu)*1.06
+    //Soma os valores sem e com impostos de todos os níveis!
+    total = tudoIVA23 + tudoIVA13 + tudoIVA6
+    //Escreve no DOM o resultado
+    document.querySelector('#idivresult').innerHTML += `${empresa} = `
+    document.querySelector('#idivresult').innerHTML += `${total.toFixed(2)}€`
+    //PODERIA TERMINAR O LOOPING
+    //FIM
+    var lista = new Map()
+    lista.set('Luzboa', 0.0282, 0.1691)
+    alert(return[])
     
-
-    var total = consumo + impEspeEnerg + contrAudioVisu + DGEG 
-
-   
-    
-
-
-
-
 }
